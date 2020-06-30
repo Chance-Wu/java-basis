ddcf45e06fa1605cb272c0fa16e4a395117ccd26
chancewu <chancewu@aliyun.com>
2020-06-30 16:55:50 +0800

    communication method between threads: 1) Synchronization

---
+++ src/main/java/com/chance/basis/thread/communicate/ThreadA.java
@@ -0,0 +1,23 @@
+package com.chance.basis.thread.communicate;
+
+/**
+ * <p>
+ *
+ * <p>
+ *
+ * @author chance
+ * @since 2020-06-30
+ */
+public class ThreadA extends Thread {
+    private MyObject object;
+
+    @Override
+    public void run() {
+        super.run();
+        object.methodA();
+    }
+
+    public ThreadA(MyObject object) {
+        this.object = object;
+    }
+}
---
+++ src/main/java/com/chance/basis/thread/communicate/ThreadB.java
@@ -0,0 +1,24 @@
+package com.chance.basis.thread.communicate;
+
+/**
+ * <p>
+ *
+ * <p>
+ *
+ * @author chance
+ * @since 2020-06-30
+ */
+public class ThreadB extends Thread {
+
+    private MyObject object;
+
+    @Override
+    public void run() {
+        super.run();
+        object.methodB();
+    }
+
+    public ThreadB(MyObject object) {
+        this.object = object;
+    }
+}
