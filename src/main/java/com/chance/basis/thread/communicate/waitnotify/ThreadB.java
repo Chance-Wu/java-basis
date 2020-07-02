f2f718d961f2d05ba5d64287f8b28a33c24295b0
chancewu <chancewu@aliyun.com>
2020-07-02 08:41:38 +0800

    communication method between threads: 3) wait/notify

---
+++ src/main/java/com/chance/basis/thread/communicate/waitnotify/ThreadB.java
@@ -0,0 +1,38 @@
+package com.chance.basis.thread.communicate.waitnotify;
+
+/**
+ * <p>
+ *
+ * <p>
+ *
+ * @author chance
+ * @since 2020-07-01
+ */
+public class ThreadB extends Thread {
+
+    private Object lock;
+
+    public ThreadB(Object lock) {
+        super();
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        try {
+            synchronized (lock) {
+                for (int i = 0; i < 10; i++) {
+                    MyList.add();
+                    if (MyList.size() == 5) {
+                        lock.notify();
+                        System.out.println("已经发出了通知");
+                    }
+                    System.out.println("添加了" + (i + 1) + "个元素!");
+                    Thread.sleep(1000);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
