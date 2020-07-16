6e0733b97949e63c164c11ac35be37a6970786ca
chancewu <chancewu@aliyun.com>
2020-07-16 16:43:55 +0800

    创建线程的3种方式及对比

--- src/main/java/com/chance/basis/thread/create/CallableTest.java
+++ src/main/java/com/chance/basis/thread/create/CallableTest.java
@@ -1,6 +1,9 @@
 package com.chance.basis.thread.create;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * <p>
@@ -16,13 +19,23 @@ public class CallableTest implements Callable<String> {
      * 任务
      */
     @Override
-    public String call() throws Exception {
+    public String call() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("循环变量i的值" + i);
+        }
+        return "结束";
+    }
+
+    public static void main(String[] args) {
+        CallableTest callableTest = new CallableTest();
+        FutureTask<String> futureTask = new FutureTask<>(callableTest);
+        new Thread(futureTask).start();
         try {
-            String a = "return String";
-            return a;
-        } catch (Exception e) {
+            System.out.println("线程的返回值：" + futureTask.get());
+        } catch (InterruptedException e) {
             e.printStackTrace();
-            return "exception";
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
     }
 }
