f9f410b0c4d8fd9161edea4a83bba640e94574fd
chancewu <chancewu@aliyun.com>
2020-07-17 14:44:41 +0800

    Java面向对象设计的六大原则

---
+++ src/main/java/com/chance/basis/principles/LODTest.java
@@ -0,0 +1,18 @@
+package com.chance.basis.principles;
+
+/**
+ * <p>
+ * 迪米特原则——LOD
+ * <p>
+ *
+ * @author chance
+ * @since 2020-07-17
+ */
+public class LODTest {
+
+    /**
+     * 定义：一个对象应该对其他对象保持最小的了解
+     *
+     *
+     */
+}
