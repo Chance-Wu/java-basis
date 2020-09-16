d83dfb03b838e98fa3561ace7a3212cc3eddd427
chancewu <chancewu@aliyun.com>
2020-09-16 10:26:18 +0800

    方法和构造函数的便捷引用

--- src/main/java/com/chance/java8/MethodReference.java
+++ src/main/java/com/chance/java8/MethodReference.java
@@ -15,12 +15,7 @@ public class MethodReference {
 
     public static void main(String[] args) {
         //返回一个抽象路径名数组，表示由此抽象路径名表示的满足指定过滤器的目录中的文件和目录
-        File[] hiddenFiles = new File(".").listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File file) { //接受一个File，返回一个布尔值
-                return file.isHidden();
-            }
-        });
+        File[] hiddenFiles = new File(".").listFiles(file -> file.isHidden());
 
         //Java 8的实现方法
         File[] files = new File(".").listFiles(File::isHidden);
