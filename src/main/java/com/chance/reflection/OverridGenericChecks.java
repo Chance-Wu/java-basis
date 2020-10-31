2c13270dbab424cedc9c9f3c3eb2c39a31f54c6f
chancewu <chancewu@aliyun.com>
2020-10-31 15:50:39 +0800

    reflection

--- src/main/java/com/chance/reflection/OverridGenericChecks.java
+++ src/main/java/com/chance/reflection/OverridGenericChecks.java
@@ -1,6 +1,5 @@
 package com.chance.reflection;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
---
+++ src/main/java/com/chance/reflection/Test.java
@@ -0,0 +1,14 @@
+package com.chance.reflection;
+
+/**
+ * @Description: Test
+ * @Author: chance
+ * @Date: 2020-10-30 16:23
+ * @Version 1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
